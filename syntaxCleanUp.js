ff8f7cbeab2cd057e236cb016b5b0c31d97b0c9c
Taianne Lima <rtaianne@gmail.com>
2023-11-23 11:32:29 -0800

    Initial Commit

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -20,3 +20,44 @@ var post2 =
       numberLikes: 13,
       comments: []
 }
+
+
+//Syntax code1
+
+var foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
+};
+
+//Syntax code 2
+
+var person = {
+  height: "6 ft" ,
+  weight: "160 lbs"
+};
+
+
+// Syntax code 3
+
+var post = {
+ imageSrc: " ./images/beach.png",
+ caption: "At the beach with my besties",
+ timestamp: "4:37 PM August 13, 2019",
+ numberLikes: 0,
+ comments: []
+};
+
+Syntax code 4
+
+var post2 = {
+
+imageSrc: "./images/holiday-party.png",
+caption: "What a great holiday party omg",
+timestamp: "11:37 PM December 31, 2019",
+numberLikes: 13,
+comments: []
+};
+
+
+
